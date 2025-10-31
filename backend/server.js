6f9a6eeeb2f621f7a4e8d5034420061b54c79528
Farahoz <farahugaas0@gmail.com>
2025-10-31 22:58:19 +0300

    Fix Footer import case issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = 8000
 
 
 app.use(cors({
-  origin: 'https://librarysystem-r0al.onrender.com/', // frontend origin
+  origin: 'https://librarysystem-r0al.onrender.com', // frontend origin
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // haddii aad isticmaalayso cookies / credentials
